9fe84186936858ef0159afb31334e58cdf9f823d
Anton Yakovenko <yakovenkoanton2007@gmail.com>
2026-05-10 02:05:38 +0200

    style: Change view of the imports

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -2,7 +2,15 @@ from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from cinema.models import Actor, CinemaHall, Genre, Movie, MovieSession, Order, Ticket
+from cinema.models import (
+    Actor,
+    CinemaHall,
+    Genre,
+    Movie,
+    MovieSession,
+    Order,
+    Ticket
+)
 
 
 class GenreSerializer(serializers.ModelSerializer):
